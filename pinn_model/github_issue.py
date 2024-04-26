8bc45ac4e38734935054d0dfc15fc92e43285b4e
Stepanov Igor <ygrik2003@yandex.ru>
2024-04-26 15:16:07 +0300

    B

--- pinn_model/github_issue.py
+++ pinn_model/github_issue.py
@@ -1,199 +1,269 @@
-from deepxde.icbc import DirichletBC
-from deepxde.geometry.geometry_2d import Polygon
-from deepxde.callbacks import EarlyStopping
-from deepxde.nn import FNN
-from deepxde.data.pde import PDE
-from deepxde.model import Model
-from deepxde.backend import tf
-import numpy as np
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch, paddle"""
 import deepxde as dde
+import numpy as np
+
 import matplotlib.pyplot as plt
-import torch
+from mpl_toolkits.mplot3d import Axes3D
+
+a = 1
+d = 1
+Re = 1
 
 
-def wall_top_boundary(x, on_boundary):
-    """Checks for points on top wall boundary"""
-    return on_boundary and np.isclose(x[1], 2.0)
+def pde(x, u):
+    u_vel, v_vel, w_vel, p = u[:, 0:1], u[:, 1:2], u[:, 2:3], u[:, 3:4]
 
-
-def wall_bottom_boundary(x, on_boundary):
-    """Checks for points on bottom wall boundary"""
-    return on_boundary and np.isclose(x[1], 0.0)
-
-
-def wall_mid_horizontal_boundary(x, on_boundary):
-    """Check for points on step horizontal boundary"""
-    return on_boundary and (np.isclose(x[1], 1.0) and x[0] < 2.0)
-
-
-def wall_mid_vertical_boundary(x, on_boundary):
-    """Check for points on step horizontal boundary"""
-    return on_boundary and (x[1] < 1.0 and np.isclose(x[0], 2.0))
-
-
-def outlet_boundary(x, on_boundary):
-    """Implements the outlet boundary with zero y-velocity component"""
-    return on_boundary and np.isclose(x[0], 12.0)
-
-
-def inlet_boundary(x, on_boundary):
-    """Implements the inlet boundary with parabolic x-velocity component"""
-    return on_boundary and np.isclose(x[0], 0.0)
-
-def boundary_circle(x, on_boundary):
-    #centre (4, 1), radius 0.3
-    return on_boundary and disk_domain.on_boundary(x)
-
-
-def parabolic_velocity(x):
-    """Parabolic velocity"""
-    return (6 * (x[:, 1] - 1) * (2 - x[:, 1])).reshape(-1, 1)
-
-
-def zero_velocity(x):
-    """Zero velocity"""
-    return np.zeros((x.shape[0], 1))
-
-
-
-
-def navier_stokes(x, u):
-    #x - x, y coordinates
-    #u - u, v, p
-    rho = 1
-    mu = 0.01
-    eps = 1e-8
-    u_vel, v_vel, p = u[:, 0:1], u[:, 1:2], u[:, 2:]
     u_vel_x = dde.grad.jacobian(u, x, i=0, j=0)
     u_vel_y = dde.grad.jacobian(u, x, i=0, j=1)
+    u_vel_z = dde.grad.jacobian(u, x, i=0, j=2)
+    u_vel_t = dde.grad.jacobian(u, x, i=0, j=3)
     u_vel_xx = dde.grad.hessian(u, x, component=0, i=0, j=0)
     u_vel_yy = dde.grad.hessian(u, x, component=0, i=1, j=1)
+    u_vel_zz = dde.grad.hessian(u, x, component=0, i=2, j=2)
 
     v_vel_x = dde.grad.jacobian(u, x, i=1, j=0)
     v_vel_y = dde.grad.jacobian(u, x, i=1, j=1)
+    v_vel_z = dde.grad.jacobian(u, x, i=1, j=2)
+    v_vel_t = dde.grad.jacobian(u, x, i=1, j=3)
     v_vel_xx = dde.grad.hessian(u, x, component=1, i=0, j=0)
     v_vel_yy = dde.grad.hessian(u, x, component=1, i=1, j=1)
+    v_vel_zz = dde.grad.hessian(u, x, component=1, i=2, j=2)
 
-    p_x = dde.grad.jacobian(u, x, i=2, j=0)
-    p_y = dde.grad.jacobian(u, x, i=2, j=1)
+    w_vel_x = dde.grad.jacobian(u, x, i=2, j=0)
+    w_vel_y = dde.grad.jacobian(u, x, i=2, j=1)
+    w_vel_z = dde.grad.jacobian(u, x, i=2, j=2)
+    w_vel_t = dde.grad.jacobian(u, x, i=2, j=3)
+    w_vel_xx = dde.grad.hessian(u, x, component=2, i=0, j=0)
+    w_vel_yy = dde.grad.hessian(u, x, component=2, i=1, j=1)
+    w_vel_zz = dde.grad.hessian(u, x, component=2, i=2, j=2)
+
+    p_x = dde.grad.jacobian(u, x, i=3, j=0)
+    p_y = dde.grad.jacobian(u, x, i=3, j=1)
+    p_z = dde.grad.jacobian(u, x, i=3, j=2)
 
     momentum_x = (
-        rho * (u_vel * u_vel_x + v_vel * u_vel_y) + p_x - mu * (u_vel_xx + u_vel_yy)
+        u_vel_t
+        + (u_vel * u_vel_x + v_vel * u_vel_y + w_vel * u_vel_z)
+        + p_x
+        - 1 / Re * (u_vel_xx + u_vel_yy + u_vel_zz)
     )
     momentum_y = (
-        rho * (u_vel * v_vel_x + v_vel * v_vel_y) + p_y - mu * (v_vel_xx + v_vel_yy)
+        v_vel_t
+        + (u_vel * v_vel_x + v_vel * v_vel_y + w_vel * v_vel_z)
+        + p_y
+        - 1 / Re * (v_vel_xx + v_vel_yy + v_vel_zz)
     )
-    continuity = u_vel_x + v_vel_y
+    momentum_z = (
+        w_vel_t
+        + (u_vel * w_vel_x + v_vel * w_vel_y + w_vel * w_vel_z)
+        + p_z
+        - 1 / Re * (w_vel_xx + w_vel_yy + w_vel_zz)
+    )
+    continuity = u_vel_x + v_vel_y + w_vel_z
 
-    return [momentum_x, momentum_y, continuity]
-
-def output_transformer(inputs, outputs):
-    """Apply output transforms to strictly enforce boundary conditions"""
-
-    top_line = inputs[:, 1] - 2.0
-    bottom_line = inputs[:, 1]
-    mid_hor_line = inputs[:, 1] - 1.0
-    mid_ver_line = inputs[:, 0] - 2.0
-    outlet_line = inputs[:, 0] - 12.0
-    inlet_line = inputs[:, 0]
-    velocity_profile = 6.0 * (inputs[:, 1] - 1.0) * (2.0 - inputs[:, 1])
-    # velocity_profile = 1.0
-
-    u_multiplier = (top_line * bottom_line * mid_hor_line * mid_ver_line *
-                    velocity_profile)
-    v_multiplier = (top_line * bottom_line * mid_hor_line * mid_ver_line *
-                    outlet_line * inlet_line)
-    p_multiplier = 1.0
-
-    return torch.cat([(u_multiplier * outputs[:, 0]).reshape(-1, 1), (v_multiplier * outputs[:, 1]).reshape(-1, 1), (p_multiplier * outputs[:, 2]).reshape(-1, 1)], dim=1)
+    return [momentum_x, momentum_y, momentum_z, continuity]
 
 
-
-if __name__ == '__main__':
-    """
-    Geometry
-    --------
-                (0, 2)       (12, 2)
-                  *------------*
-        in -> |            |
-        (0, 1)*--*(2,1)    . -> out
-                    |         |
-            (2,0)*---------*(12, 0)
-    """
-    geom = Polygon([
-        [0.0, 2.0], [12.0, 2.0], [12.0, 0.0], [2.0, 0.0], [2.0, 1.0],
-        [0.0, 1.0]
-    ])
-    disk_domain = dde.geometry.Disk([4, 1], 0.3)
-    geom = geom - disk_domain
-
-    inlet_x = DirichletBC(geom, parabolic_velocity, inlet_boundary,
-                          component=0)
-    inlet_y = DirichletBC(geom, zero_velocity, inlet_boundary, component=1)
-    outlet = DirichletBC(geom, zero_velocity, outlet_boundary, component=1)
-    wallt_x = DirichletBC(geom, zero_velocity, wall_top_boundary, component=0)
-    wallt_y = DirichletBC(geom, zero_velocity, wall_top_boundary, component=1)
-    wallb_x = DirichletBC(geom, zero_velocity, wall_bottom_boundary,
-                          component=0)
-    wallb_y = DirichletBC(geom, zero_velocity, wall_bottom_boundary,
-                          component=1)
-    wallsh_x = DirichletBC(geom, zero_velocity, wall_mid_horizontal_boundary,
-                           component=0)
-    wallsh_y = DirichletBC(geom, zero_velocity, wall_mid_horizontal_boundary,
-                           component=1)
-    wallsv_x = DirichletBC(geom, zero_velocity, wall_mid_vertical_boundary,
-                           component=0)
-    wallsv_y = DirichletBC(geom, zero_velocity, wall_mid_vertical_boundary,
-                           component=1)
-    circle = DirichletBC(geom, zero_velocity, boundary_circle)
-
-
-    data = PDE(
-        geom, navier_stokes,
-        [inlet_x, inlet_y, outlet, wallt_x, wallt_y, wallb_x,
-        wallb_x, wallsh_x, wallsh_y, wallsv_x, wallsv_y, circle], num_domain=50000,
-        num_boundary=50000, num_test=10000
+def u_func(x):
+    return (
+        -a
+        * (
+            np.exp(a * x[:, 0:1]) * np.sin(a * x[:, 1:2] + d * x[:, 2:3])
+            + np.exp(a * x[:, 2:3]) * np.cos(a * x[:, 0:1] + d * x[:, 1:2])
+        )
+        * np.exp(-(d ** 2) * x[:, 3:4])
     )
 
-    layer_size = [2] + [50] * 6 + [3]
-    net = FNN(layer_size, "tanh", "Glorot uniform")
-    # net.apply_output_transform(output_transformer)
 
-    model = Model(data, net)
-    model.compile("adam", lr=0.001)
-
-    # early_stopping = EarlyStopping(min_delta=1e-8, patience=40000)
-    # checker = dde.callbacks.ModelCheckpoint("model/bend_model.ckpt", save_better_only=True, period=1000)
-    # model.train(iterations=5000, display_every=1000, callbacks=[checker])
-
-    model.compile("L-BFGS")
-    # losshistory, train_state = model.train(model_save_path = "./model/bend_model.ckpt")
-
-    # model.restore("model/bend_model.ckpt-20000.pt", verbose=1)  # Replace ? with the exact filename
-    X = geom.random_points(100000)
-    output = model.predict(X)
-
-    u_pred = output[:, 0]
-    v_pred = output[:, 1]
-    velocity_pred = np.sqrt(u_pred**2 + v_pred**2)
-    p_pred = output[:, 2]
-
-    fig = plt.figure(figsize=(12,2))
-    plt.scatter(X[:, 0], X[:, 1], c=velocity_pred, cmap='jet',vmin=0, vmax=1, marker = '.')
-    plt.colorbar().ax.set_ylabel('z', rotation=270)
+def v_func(x):
+    return (
+        -a
+        * (
+            np.exp(a * x[:, 1:2]) * np.sin(a * x[:, 2:3] + d * x[:, 0:1])
+            + np.exp(a * x[:, 0:1]) * np.cos(a * x[:, 1:2] + d * x[:, 2:3])
+        )
+        * np.exp(-(d ** 2) * x[:, 3:4])
+    )
 
 
-    #residual plot 
-    f = model.predict(X, operator=navier_stokes)
-    print("f shape:", f[0].shape)
-    print("X shape:", X.shape)
+def w_func(x):
+    return (
+        -a
+        * (
+            np.exp(a * x[:, 2:3]) * np.sin(a * x[:, 0:1] + d * x[:, 1:2])
+            + np.exp(a * x[:, 1:2]) * np.cos(a * x[:, 2:3] + d * x[:, 0:1])
+        )
+        * np.exp(-(d ** 2) * x[:, 3:4])
+    )
 
-    fig = plt.figure(figsize=(12,2))
-    plt.scatter(X[:, 0], X[:, 1], c=f[0], cmap='jet',vmin=0, vmax=1, marker = '.')
-    plt.colorbar().ax.set_ylabel('z', rotation=270)
-    plt.show()
+
+def p_func(x):
+    return (
+        -0.5
+        * a ** 2
+        * (
+            np.exp(2 * a * x[:, 0:1])
+            + np.exp(2 * a * x[:, 1:2])
+            + np.exp(2 * a * x[:, 2:3])
+            + 2
+            * np.sin(a * x[:, 0:1] + d * x[:, 1:2])
+            * np.cos(a * x[:, 2:3] + d * x[:, 0:1])
+            * np.exp(a * (x[:, 1:2] + x[:, 2:3]))
+            + 2
+            * np.sin(a * x[:, 1:2] + d * x[:, 2:3])
+            * np.cos(a * x[:, 0:1] + d * x[:, 1:2])
+            * np.exp(a * (x[:, 2:3] + x[:, 0:1]))
+            + 2
+            * np.sin(a * x[:, 2:3] + d * x[:, 0:1])
+            * np.cos(a * x[:, 1:2] + d * x[:, 2:3])
+            * np.exp(a * (x[:, 0:1] + x[:, 1:2]))
+        )
+        * np.exp(-2 * d ** 2 * x[:, 3:4])
+    )
 
 
-    residual = np.mean(np.absolute(f))
+spatial_domain = dde.geometry.Cuboid(xmin=[-1, -1, -1], xmax=[1, 1, 1])
+temporal_domain = dde.geometry.TimeDomain(0, 1)
+spatio_temporal_domain = dde.geometry.GeometryXTime(spatial_domain, temporal_domain)
 
-    print("Mean residual:", residual)
+boundary_condition_u = dde.icbc.DirichletBC(
+    spatio_temporal_domain, u_func, lambda _, on_boundary: on_boundary, component=0
+)
+boundary_condition_v = dde.icbc.DirichletBC(
+    spatio_temporal_domain, v_func, lambda _, on_boundary: on_boundary, component=1
+)
+boundary_condition_w = dde.icbc.DirichletBC(
+    spatio_temporal_domain, w_func, lambda _, on_boundary: on_boundary, component=2
+)
+
+initial_condition_u = dde.icbc.IC(
+    spatio_temporal_domain, u_func, lambda _, on_initial: on_initial, component=0
+)
+initial_condition_v = dde.icbc.IC(
+    spatio_temporal_domain, v_func, lambda _, on_initial: on_initial, component=1
+)
+initial_condition_w = dde.icbc.IC(
+    spatio_temporal_domain, w_func, lambda _, on_initial: on_initial, component=2
+)
+
+data = dde.data.TimePDE(
+    spatio_temporal_domain,
+    pde,
+    [
+        boundary_condition_u,
+        boundary_condition_v,
+        boundary_condition_w,
+        initial_condition_u,
+        initial_condition_v,
+        initial_condition_w,
+    ],
+    num_domain=50000,
+    num_boundary=5000,
+    num_initial=5000,
+    num_test=10000,
+)
+
+net = dde.nn.FNN([4] + 4 * [50] + [4], "tanh", "Glorot normal")
+
+model = dde.Model(data, net)
+
+model.compile("adam", lr=1e-3, loss_weights=[1, 1, 1, 1, 100, 100, 100, 100, 100, 100])
+model.train(iterations=30000, display_every=1)
+model.compile("L-BFGS", loss_weights=[1, 1, 1, 1, 100, 100, 100, 100, 100, 100])
+losshistory, train_state = model.train()
+
+x, y, z = np.meshgrid(
+    np.linspace(-1, 1, 10), np.linspace(-1, 1, 10), np.linspace(-1, 1, 10)
+)
+
+X = np.vstack((np.ravel(x), np.ravel(y), np.ravel(z))).T
+
+t_0 = np.zeros(1000).reshape(1000, 1)
+t_1 = np.ones(1000).reshape(1000, 1)
+
+X_0 = np.hstack((X, t_0))
+X_1 = np.hstack((X, t_1))
+
+output_0 = model.predict(X_0)
+output_1 = model.predict(X_1)
+
+u_pred_0 = output_0[:, 0].reshape(-1)
+v_pred_0 = output_0[:, 1].reshape(-1)
+w_pred_0 = output_0[:, 2].reshape(-1)
+p_pred_0 = output_0[:, 3].reshape(-1)
+
+u_exact_0 = u_func(X_0).reshape(-1)
+v_exact_0 = v_func(X_0).reshape(-1)
+w_exact_0 = w_func(X_0).reshape(-1)
+p_exact_0 = p_func(X_0).reshape(-1)
+
+u_pred_1 = output_1[:, 0].reshape(-1)
+v_pred_1 = output_1[:, 1].reshape(-1)
+w_pred_1 = output_1[:, 2].reshape(-1)
+p_pred_1 = output_1[:, 3].reshape(-1)
+
+u_exact_1 = u_func(X_1).reshape(-1)
+v_exact_1 = v_func(X_1).reshape(-1)
+w_exact_1 = w_func(X_1).reshape(-1)
+p_exact_1 = p_func(X_1).reshape(-1)
+
+f_0 = model.predict(X_0, operator=pde)
+f_1 = model.predict(X_1, operator=pde)
+
+l2_difference_u_0 = dde.metrics.l2_relative_error(u_exact_0, u_pred_0)
+l2_difference_v_0 = dde.metrics.l2_relative_error(v_exact_0, v_pred_0)
+l2_difference_w_0 = dde.metrics.l2_relative_error(w_exact_0, w_pred_0)
+l2_difference_p_0 = dde.metrics.l2_relative_error(p_exact_0, p_pred_0)
+residual_0 = np.mean(np.absolute(f_0))
+
+l2_difference_u_1 = dde.metrics.l2_relative_error(u_exact_1, u_pred_1)
+l2_difference_v_1 = dde.metrics.l2_relative_error(v_exact_1, v_pred_1)
+l2_difference_w_1 = dde.metrics.l2_relative_error(w_exact_1, w_pred_1)
+l2_difference_p_1 = dde.metrics.l2_relative_error(p_exact_1, p_pred_1)
+residual_1 = np.mean(np.absolute(f_1))
+
+print("Accuracy at t = 0:")
+print("Mean residual:", residual_0)
+print("L2 relative error in u:", l2_difference_u_0)
+print("L2 relative error in v:", l2_difference_v_0)
+print("L2 relative error in w:", l2_difference_w_0)
+print("\n")
+print("Accuracy at t = 1:")
+print("Mean residual:", residual_1)
+print("L2 relative error in u:", l2_difference_u_1)
+print("L2 relative error in v:", l2_difference_v_1)
+print("L2 relative error in w:", l2_difference_w_1)
+
+# Визуализация решения
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title('Solution of Navier-Stokes Equations')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+X, Y, Z = np.meshgrid(np.linspace(-1, 1, 10), np.linspace(-1, 1, 10), np.linspace(-1, 1, 10))
+
+u_pred_0 = output_0[:, 0].reshape(X.shape)
+v_pred_0 = output_0[:, 1].reshape(X.shape)
+w_pred_0 = output_0[:, 2].reshape(X.shape)
+
+u_pred_1 = output_1[:, 0].reshape(X.shape)
+v_pred_1 = output_1[:, 1].reshape(X.shape)
+w_pred_1 = output_1[:, 2].reshape(X.shape)
+
+# Визуализация решения для t = 0
+ax.quiver(X, Y, Z, u_pred_0, v_pred_0, w_pred_0, length=0.1, normalize=True, color='r')
+ax.set_title('Solution at t = 0')
+plt.show()
+
+# Визуализация решения для t = 1
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title('Solution of Navier-Stokes Equations')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.quiver(X, Y, Z, u_pred_1, v_pred_1, w_pred_1, length=0.1, normalize=True, color='r')
+ax.set_title('Solution at t = 1')
+plt.show()
